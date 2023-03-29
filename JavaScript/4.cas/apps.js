65a689a2d28a97f1b0bfc4e3696b18ae27c36fda
marijasimovic <marijasimovic2005@gmail.com>
2023-03-29 22:59:44 +0200

    cas

---
+++ JavaScript/4.cas/apps.js
@@ -0,0 +1,21 @@
+//kondicionali
+
+if ("prvi uslov") {
+  //block kod za izvrsavanje naredbe
+} else if ("drugi") {
+} else {
+  //ako if nije tacno
+}
+const broj_godina = 25;
+if (broj_godina >= 18) {
+  console.log("punoletni ste");
+} else {
+  console.log("maloletni ste");
+}
+let x = 5;
+let y = 2;
+if (y === 0) {
+  console.log("poruka");
+} else {
+  console.log(x / y);
+}
