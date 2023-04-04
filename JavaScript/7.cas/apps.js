1422769e6046a9c1be5dc211d850b4d31e69a2bc
marijasimovic <marijasimovic2005@gmail.com>
2023-04-04 20:51:16 +0200

    domaci

--- JavaScript/7.cas/apps.js
+++ JavaScript/7.cas/apps.js
@@ -2,6 +2,14 @@ for (let i = 1; i < 20; i += 2) {
   console.log(i);
 }
 
-// for(let i=6; ){
-
-// }
+for (i = 50; i < 101; i += 5) {
+  console.log(i);
+}
+for (i = 6; i < 15; i += i) {
+  console.log(i);
+}
+s = 0;
+for (let i = 10; i < 20; i += 2) {
+  s += i;
+}
+console.log(s);
