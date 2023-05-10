0f6426ebadf2f6b6aefb82b1c31837943d471ca4
marijasimovic <marijasimovic2005@gmail.com>
2023-05-10 21:18:36 +0200

    cas

---
+++ JavaScript/15.cas/apps2.js
@@ -0,0 +1,29 @@
+//STRINGOVI
+//Su immutable vrednosti
+//oni su primitivnog(osnovnog)tipa podataka
+
+//za dobijanje duzine nekog stringa koristimo lenght metodu
+let a="string"
+duzina=a.length
+
+
+
+//stringove mozemo zapisati na nekoliko nacina
+
+//1.
+//koristeci duple navodnike
+"danas je lep dan"
+//2.
+//koristeci obicne navodnike
+'promenljivo vreme'
+//3.template literals(back-tics sintaksa)
+`lose vreme`
+
+//dupli navodnici
+"danas je bilo"promenljivo" vreme"
+"danas je bilo\"promenljivo\" vreme"
+
+
+//obicni navodnici
+"danas je bilo"promenljivo" vreme"
+"danas je bilo\"promenljivo\" vreme"
